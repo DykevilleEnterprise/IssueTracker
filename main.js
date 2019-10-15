d282c8e5b83091ea4f017c03ae656838ac304ecd
DykevilleEnterprise <dikeojoikechukwu@gmail.com>
2019-10-15 13:26:56 +0100

    Third commit for main.js

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
+document.getElementById('issueInputForm').addEventListener('submit', saveIssue);
+
+function saveIssue(e){
+    var issueDesc = document.getElementById('issueDescInput').Value;
+    var issueSeverity = document.getElementById('issueSeverityInput').Value;
+    var issueAssignedTo = document.getElementById('issueAssignedToInput').Value;
+    var issueId = chance.guid();
+    var issueStatus = 'Open';
+
+    var issue ={
+        id: issueId,
+        description: issueDesc, 
+        severity: issueSeverity,
+        assignedTo: issueAssignedTo,
+        status: issueStatus,
+    }
+
+    if (localStorage.getItem('issues') == null) {
+        var issues = [];
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    } else {
+        var issues = JSON.parse(localStorage.getItem('issues'));
+        issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
+    }
+
+    
+}
+
 function fetchIssues(){
     var issues = JSON.parse(localStorage.getItem('issues'));
     var issuesListe = document.getElementById('issuesList');
